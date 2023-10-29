fa973d550bb09708d87d774c3533c5e44d95f8d7
Aakib Kibria Khan <96371512+airbik-codes@users.noreply.github.com>
2023-10-29 19:19:35 -0400

    Add files via upload

---
+++ lab1b.py
@@ -0,0 +1,10 @@
+'''
+Name: Aakib Kibria Khan
+Student ID: 157802224
+'''
+print("Welcome to PRG600!")
+username=input("Enter your name: ")
+student_ID=input("Enter your student ID: ")
+age=input("Enter your age: ")
+favorite_food=input("Enter your favorite food: ")
+print(f"{username} is {age} years old and loves to eat {favorite_food}!\n Goodluck with the course!")
