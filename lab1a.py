b20b44fced6d1bd223cc6987324b5a1af6643104
Aakib Kibria Khan <96371512+airbik-codes@users.noreply.github.com>
2023-10-29 19:19:10 -0400

    Add files via upload

---
+++ lab1a.py
@@ -0,0 +1,9 @@
+'''
+Name: Aakib Kibria Khan
+Student ID: 157802224
+Description: This is our first program.
+'''
+
+print("Hello World!")
+user_name = input("Please enter your name: ") #ask user for input
+print("Hello " + user_name + "!") #prints username in a friendly way.
