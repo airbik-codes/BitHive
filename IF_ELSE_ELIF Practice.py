f16b1fa0b90c6324916f8c606c8a569c114a79fc
Aakib Kibria Khan <akkhan@protonmail.com>
2023-12-06 14:11:47 -0500

    Add files via upload

---
+++ IF_ELSE_ELIF Practice.py
@@ -0,0 +1,42 @@
+# user_input=input("Please enter an integer to check whether it is positive, negative or zero: ")
+# if int(user_input)== 0:
+#     print("It's zero. ")
+# elif int(user_input)>0:
+#     print("It's a positive integer.")
+# elif int(user_input)<0:
+#     print("It's a negative integer.")
+
+# user_input=input("Enter a number to check whether it is even or odd: ")
+# num= int(user_input)
+
+# if num%2==0:
+#     print("It's even. ")
+# else:
+#     print("It's odd.")
+
+# user_input= input("Please enter the first integer: ")
+# num1= int(user_input)
+# user_input= input("Please enter the second integer: ")
+# num2= int(user_input)
+
+# if num1==num2:
+#     print("The numbers entered are equal. ")
+# elif num2>num1:
+#     print("The second number entered is greater than the first number. ")
+# else:
+#     print("The first number entered is greater than the second number. ")
+
+# year=int(input("Please enter an year to check whether it is a leap year: "))
+
+# if year%4==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print("It's a leap year.")
+#         else:
+#             print("It's not a leap year.")
+    
+#     else:
+#         print("It's a leap year.")
+# else:
+#     print("It's not a leap year.")
+    
