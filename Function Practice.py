3dc922e21bfe86bfa23a42d117fb78fe0e3a5c72
Aakib Kibria Khan <akkhan@protonmail.com>
2023-12-17 18:33:14 -0500

    Add files via upload

---
+++ Function Practice.py
@@ -0,0 +1,75 @@
+# def add_num(a,b):
+#     c=a+b
+#     return c
+# a=int(input("enter an integer"))
+# b= int(input("enter another integer"))
+
+# print("the sum of the integers is ", add_num(a,b))
+
+
+# def area_calculator(length,width):
+#     area=length*width
+#     return area
+
+# length= float(input("enter the length of the rectangle: "))
+# width=float(input("enter the width of the rectangle: "))
+
+# print("The area of the rectangle is: ", round(area_calculator(length,width),2))
+
+# def is_even(a):
+    
+#     if a%2==0:
+#         print("The number is even.")
+#     else:
+#         print("It is odd. ")
+    
+        
+# a=float(input("enter a number to check whethe it is even or odd: "))
+
+# print(is_even(a))
+
+# def sum_number(list_of_numbers):
+#     total=0
+#     for number in list_of_numbers:
+#         total=total+number
+#     return total
+# list_of_numbers=[1,2,3,4,5,6,7,8,9]
+# print(sum_number(list_of_numbers))
+
+
+# def max_number(list_of_numbers):
+#     largest=list_of_numbers[0]
+#     for i in range(0,len(list_of_numbers)):
+#         if list_of_numbers[i]>largest:
+#             largest=list_of_numbers[i]
+#     return largest
+
+# list_of_numbers=[0,100,2,34,3]
+# print(max_number(list_of_numbers))
+
+# def reverse_string(x):
+#     reversed=x[::-1]
+#     return reversed
+# x=input("Enter a string that you want to reverse: ")
+# reversed_output= reverse_string(x)
+# print(reversed_output)
+
+# for numbers in range(0,6):
+#     print(numbers)
+
+# total=0
+# for numbers in range(1,6):
+#     total=total+numbers
+# print(total)
+
+# for numbers in range(1,5):
+#     print(numbers**2)
+# even=[]
+# for numbers in range(1,11):
+    
+#     if numbers%2==0:
+#         print(numbers)
+#         even.append(numbers)
+    
+# print(len(even))
+        
